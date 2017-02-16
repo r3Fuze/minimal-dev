8ff21950d955f2533ff8a0347d4df4a86358b331
Morten Lindhardt <r3fuze@gmail.com>
2017-02-16 01:17:07 +0100

    Starting project

---
+++ test.js
@@ -0,0 +1,13 @@
+import test from "ava"
+
+test("foo", t => {
+    t.pass()
+})
+
+test("bar", async t => {
+    const bar = Promise.resolve("bar")
+
+    t.is(await bar, "bar")
+})
+
+test.todo("baz")
