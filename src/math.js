05795eed9864bbec14c1dc0d36e8a467f1c9d79d
Morten Lindhardt <r3fuze@gmail.com>
2017-02-17 13:29:21 +0100

    docs(docs): More documentation

--- src/math.js
+++ src/math.js
@@ -20,6 +20,12 @@ export const subtract = (a, b) => {
     return a - b
 }
 
+/**
+ * Multiply two numbers
+ * @param  {Number} a The first number
+ * @param  {Number} b The second number
+ * @return {Number}   The result of multiplying the two numbers
+ */
 export const multiply = (a, b) => {
     return a * b
 }
