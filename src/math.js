810d30a9e1c981ed56f07602020bbc5c79103d62
Morten Lindhardt <r3fuze@gmail.com>
2017-02-17 04:54:05 +0100

    docs(docs): More docs

--- src/math.js
+++ src/math.js
@@ -2,13 +2,20 @@
 
 /**
  * Add two numbers
- * @param {Number} a The first number
- * @param {Number} b The second number
+ * @param  {Number} a The first number
+ * @param  {Number} b The second number
+ * @return {Number}   The sum of the two numbers
  */
 export const add = (a, b) => {
     return a + b
 }
 
+/**
+ * Subtracts two numbers
+ * @param  {Number} a The first number
+ * @param  {Number} b The second number
+ * @return {Number}   The result of subtracting the two numbers
+ */
 export const subtract = (a, b) => {
     return a - b
 }
